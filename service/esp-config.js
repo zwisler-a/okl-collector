ab0e579c40c335c729e3c38622306352f18c8f4c
Alexander Zwisler <alex-z-@web.de>
2023-09-17 20:10:20 +0200

    feat: new esp

--- service/esp-config.js
+++ service/esp-config.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const {mapConfigToEspResponse} = require("./esp-config");
 
 
 const filePath = "./data/config.json";
@@ -6,15 +7,28 @@ let config = JSON.parse(fs.readFileSync(filePath).toString());
 
 
 module.exports = {
+
+    mapConfigToEspResponse(config) {
+        return {
+            s: config.sleep_time
+        }
+    },
+
+    createConfig: (espName) => {
+        if (config[espName]) return config[espName];
+        const newConfig = {
+            sleep_time: 30
+        }
+        config[espName] = newConfig;
+        return newConfig;
+    },
+
     getConfig: (espName) => {
         if (!espName) return config;
         if (config[espName]) {
-            return {
-                s: config[espName].sleep_time
-            };
-        }
-        return {
-            s: 60
+            return this.mapConfigToEspResponse(config[espName]);
+        } else {
+            return this.mapConfigToEspResponse(this.createConfig(espName));
         }
     },
 
