52ae7edac041ddc121b5a5f18368869bb3c1fd01
Alexander Zwisler <alex-z-@web.de>
2023-09-17 21:03:13 +0200

    fix: create on missing config

--- service/esp-config.js
+++ service/esp-config.js
@@ -1,34 +1,36 @@
 const fs = require("fs");
-const {mapConfigToEspResponse} = require("./esp-config");
 
 
 const filePath = "./data/config.json";
 let config = JSON.parse(fs.readFileSync(filePath).toString());
 
+const mapConfigToEspResponse = (config) => {
+    return {
+        s: config.sleep_time
+    }
+}
+
+const createConfig = (espName) => {
+    if (config[espName]) return config[espName];
+    const newConfig = {
+        sleep_time: 30
+    }
+    config[espName] = newConfig;
+    return newConfig;
+};
+
 
 module.exports = {
+    mapConfigToEspResponse,
+    createConfig,
 
-    mapConfigToEspResponse(config) {
-        return {
-            s: config.sleep_time
-        }
-    },
-
-    createConfig: (espName) => {
-        if (config[espName]) return config[espName];
-        const newConfig = {
-            sleep_time: 30
-        }
-        config[espName] = newConfig;
-        return newConfig;
-    },
 
     getConfig: (espName) => {
         if (!espName) return config;
         if (config[espName]) {
-            return this.mapConfigToEspResponse(config[espName]);
+            return mapConfigToEspResponse(config[espName]);
         } else {
-            return this.mapConfigToEspResponse(this.createConfig(espName));
+            return mapConfigToEspResponse(createConfig(espName));
         }
     },
 
